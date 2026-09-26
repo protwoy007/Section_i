ef7b6927c44d79392b2e8a84de0832bb268a4187
Khaled Mosharraf <222888422+protwoy007@users.noreply.github.com>
2026-09-27 01:31:35 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// Fill this in with YOUR Firebase project's web config
 const firebaseConfig = {
   apiKey: "AIzaSyCxud63VO4NJYn8YQiVTNCuh9ZhApKa4rw",
   authDomain: "section-notices.firebaseapp.com",
@@ -9,5 +8,4 @@ const firebaseConfig = {
   measurementId: "G-5R7ZL8MWGX"
 };
 
-// The 2 admin emails allowed to write (must match Firestore rules)
-const ADMIN_EMAILS =  ["protwoy786@gmail.com", "sharemovies40@gmail.com"];
+const ADMIN_EMAILS =  ["protwoy786@gmail.com", "fuad.cse@gmail.com"];
